3a33c39a15528d69646ad37cdedc1512d7b38e77
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2019-08-25 23:17:56 +0300

    load contact info from json

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 
 # Create your views here.
@@ -18,8 +19,36 @@ def product(request):
 
 
 def contact(request):
+    # locations = [
+    #     {
+    #         'state': 'California',
+    #         'phone': '1900-1234-5678',
+    #         'email': 'info-ca@interior.com',
+    #         'address': '12 W 1st St, 90001 Los Angeles, California'
+    #     },
+    #     {
+    #         'state': 'Florida',
+    #         'phone': '5600-1234-5638',
+    #         'email': 'info-fl@interior.com',
+    #         'address': '38 W 3st St, 90528 Miami , Florida'
+    #     },
+    #     {
+    #         'state': 'Washington',
+    #         'phone': '1200-1484-5695',
+    #         'email': 'info-wa@interior.com',
+    #         'address': '46 W 5st St, 90853 Seattle, Washington'
+    #     },
+    # ]
+
+    # with open('geekshop/locations.json', 'w', encoding='utf-8') as f:
+    #     json.dump(locations, f)
+
+    with open('geekshop/locations.json', 'r', encoding='utf-8') as f:
+        locations = json.load(f)
+
     context = {
         'page_title': 'interior: contacts',
+        'locations': locations,
     }
     return render(request, 'mainapp/interior-contact.html', context)
 
