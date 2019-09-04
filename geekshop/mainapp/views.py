8a8223ff6d2c030bf10034572af619eb3cf113d3
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2019-09-04 22:21:55 +0300

    categories menu

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,6 @@
 import json
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import ProductCategory, Product
 
 # Create your views here.
@@ -14,11 +15,28 @@ def main(request):
 
 def product(request, pk=None):
     print(pk)
-    all_products = Product.objects.filter(category_id=pk).all()
     categories = ProductCategory.objects.all()
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all()
+            category = {'name': 'all'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk)
+        context = {
+            'page_title': 'interior: products',
+            'products': products,
+            'categories': categories,
+            'category': category,
+        }
+
+        return render(request, 'mainapp/interior-product.html', context)
+
+    products = Product.objects.all()
     context = {
         'page_title': 'interior: products',
-        'all_products': all_products,
+        'products': products,
         'categories': categories,
     }
     return render(request, 'mainapp/interior-product.html', context)
@@ -37,7 +55,9 @@ def contact(request):
 
 
 def productdet(request):
+    categories = ProductCategory.objects.all()
     context = {
         'page_title': 'interior: fishnet chair',
+        'categories': categories
     }
     return render(request, 'mainapp/interior-product-details.html', context)
