071529dd316af3028296767920201df801f0d573
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2019-09-08 12:13:58 +0300

    add product and hot product pages

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,4 +1,5 @@
 import json
+import random
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
@@ -8,11 +9,32 @@ from .models import ProductCategory, Product
 # Create your views here.
 
 
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+
+    return same_products
+
+
 def main(request):
     products = Product.objects.all()[:6]
+    basket = get_basket(request.user)
+
     context = {
         'page_title': 'interior: home page',
         'products': products,
+        'basket': basket,
     }
     return render(request, 'mainapp/index.html', context)
 
@@ -20,10 +42,7 @@ def main(request):
 def product(request, pk=None):
     print(pk)
     categories = ProductCategory.objects.all()
-
-    basket = []
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
+    basket = get_basket(request.user)
 
     if pk is not None:
         if pk == 0:
@@ -42,17 +61,22 @@ def product(request, pk=None):
 
         return render(request, 'mainapp/interior-product.html', context)
 
-    products = Product.objects.all().order_by('price')
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
+
     context = {
         'page_title': 'interior: products',
-        'products': products,
         'categories': categories,
+        'hot_product': hot_product,
+        'same_products': same_products,
         'basket': basket,
     }
-    return render(request, 'mainapp/interior-product.html', context)
+    return render(request, 'mainapp/interior-product-hot.html', context)
 
 
 def contact(request):
+
+    basket = get_basket(request.user)
 
     with open('geekshop/locations.json', 'r', encoding='utf-8') as f:
         locations = json.load(f)
@@ -60,14 +84,20 @@ def contact(request):
     context = {
         'page_title': 'interior: contacts',
         'locations': locations,
+        'basket': basket,
     }
     return render(request, 'mainapp/interior-contact.html', context)
 
 
-def productdet(request):
+def productdet(request, pk):
+    basket = get_basket(request.user)
     categories = ProductCategory.objects.all()
+    product = get_object_or_404(Product, pk=pk)
+
     context = {
-        'page_title': 'interior: fishnet chair',
-        'categories': categories
+        'page_title': 'interior: product',
+        'categories': categories,
+        'product': product,
+        'basket': basket,
     }
     return render(request, 'mainapp/interior-product-details.html', context)
