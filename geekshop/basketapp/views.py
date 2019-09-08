d3f4d7ee6921e34d5b9cc0d63c70315ec82ea959
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2019-09-08 12:36:51 +0300

    add login proposal for user

--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+
 from basketapp.models import Basket
 from mainapp.models import Product
 
 
+@login_required
 def basket(request):
     title = 'basket'
     basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
@@ -14,6 +18,7 @@ def basket(request):
     return render(request, 'basketapp/basket.html', context)
 
 
+@login_required
 def basket_add(request, pk):
     product = get_object_or_404(Product, pk=pk)
     basket = Basket.objects.filter(user=request.user, product=product).first()
@@ -27,6 +32,7 @@ def basket_add(request, pk):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove(request, pk):
     basket_record = get_object_or_404(Basket, pk=pk)
     basket_record.delete()
