54a05a82d772862dd73735b61740cc946d8ab47d
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2019-09-11 17:45:49 +0300

    create link in main menu

--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import user_passes_test
 from authapp.models import ShopUser
 from mainapp.models import Product, ProductCategory
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def users(request):
     title = 'admin/users'
 
